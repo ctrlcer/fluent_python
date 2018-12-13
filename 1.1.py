9a5325e983686576f33137e162093cf519fb688e
GaoxiongZheng <purifiedzheng@gmail.com>
2018-12-13 17:05:09 +0800

    sorted cards

--- 1.1.py
+++ 1.1.py
@@ -8,7 +8,7 @@ class FrenchDeck:
 	
 	def __init__(self):
 		self._cards=[Card(rank,suit) for suit in self.suits for rank in self.ranks]
-	
+	 
 	def __len__(self):
 		return len(self._cards)
 	
@@ -16,4 +16,38 @@ class FrenchDeck:
 		return self._cards[position]
 
 beer_card = Card('7','diamods')
+
 print(beer_card)
+
+deck = FrenchDeck()
+print(len(deck)) 
+
+print(deck[0])
+print(deck[-1])
+
+
+from random import choice
+
+print(choice(deck))
+
+print(deck[:3])
+print(deck[12::13])
+
+for card in deck:
+	print(card)
+
+for card in reversed(deck):
+	print(card)
+	
+print(Card('Q', 'hearts') in deck)
+print(Card('7', 'beasts') in deck)
+
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+def spades_high(card):
+	rank_value = FrenchDeck.ranks.index(card.rank)
+	return rank_value * len(suit_values) + suit_values[card.suit]
+	
+for card in sorted(deck, key=spades_high):
+	print(card)
+	
