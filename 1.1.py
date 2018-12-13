08cf39f52935e20d3b6f35ec907aab986f9786ee
GaoxiongZheng <purifiedzheng@gmail.com>
2018-12-13 22:02:34 +0800

    Vector model

--- 1.1.py
+++ 1.1.py
@@ -48,6 +48,7 @@ def spades_high(card):
 	rank_value = FrenchDeck.ranks.index(card.rank)
 	return rank_value * len(suit_values) + suit_values[card.suit]
 	
+
 for card in sorted(deck, key=spades_high):
 	print(card)
 	
