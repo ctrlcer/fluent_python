a0b7ea06dc7469b9f664281ca1117a657fdadb2b
GaoxiongZheng <purifiedzheng@gmail.com>
2018-12-14 14:18:51 +0800

    use tuple to record

--- 2-7.py
+++ 2-7.py
@@ -8,3 +8,35 @@ for passport in sorted(traveler_ids):
 for country, _ in traveler_ids:
 	print(country)
 
+
+latitude, longitude = lax_coordinates
+print(latitude, longitude)
+
+
+print(divmod(20, 8))
+t = (20, 8)
+print(divmod(*t))
+
+quotient, remainder = divmod(*t)
+print(quotient, remainder)
+
+import os
+
+_, filename = os.path.split('/home/luciano/.ssh/idrsa.pub')
+print(filename)
+
+a, b, *rest = range(5)
+print(a, b, rest)
+
+a, b, *rest = range(3)
+print(a, b, rest)
+
+a, b, *rest = range(2)
+print(a, b, rest)
+
+
+a, *body, c, d = range(5)
+print(a, body, c, d)
+
+*head, b, c, d = range(5)
+print(head, b, c, d)
