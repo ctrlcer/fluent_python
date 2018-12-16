07de023e7cd8920894a54bad53bae13a816f2581
GaoxiongZheng <purifiedzheng@gmail.com>
2018-12-16 16:15:34 +0800

    list

---
+++ 2-12.py
@@ -0,0 +1,24 @@
+board = [['_'] * 3 for i in range(3)]
+print(board)
+board[1][2] = 'X'
+print(board)
+
+weird_board = [['_'] * 3] * 3
+print(weird_board)
+weird_board[1][2] = 0
+print(weird_board)
+
+row = ['_'] * 3
+board = []
+for i in range(3):
+	board.append(row)
+	
+board = []
+for i in range(3):
+	row = ['_'] * 3
+	board.append(row)
+
+print(board)
+board[2][0] = 'X'
+print(board)
+
