247e37800f2b92860fcb3a8e8fa1a745c421bcb2
GaoxiongZheng <purifiedzheng@gmail.com>
2018-12-16 12:28:30 +0800

    namedtuple

--- 2-9.py
+++ 2-9.py
@@ -7,3 +7,12 @@ print(tokyo.name)
 print(tokyo.coordinate)
 print(tokyo[1])
 
+print(City._fields)
+
+LatLong = namedtuple('LatLong','lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+delhi = City._make(delhi_data)
+print(delhi._asdict())
+for key, value in delhi._asdict().items():
+	print(key + ':', value)
+
